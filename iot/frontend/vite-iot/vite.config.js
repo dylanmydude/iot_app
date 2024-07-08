7e3f0ccf7f5a646c02045aad102edb71fdd69075
dylanmydude <dylan@euca.co.za>
2024-07-08 23:08:14 +0200

    configured tailwind and removed redundant react files

--- iot/frontend/vite-iot/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../../static', // Adjusted to reflect the correct path
-  },
-});
