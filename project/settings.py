86015a75ff7d206930c677a5b4a55fe025c12189
Dylan <90636797+dylanmydude@users.noreply.github.com>
2025-01-13 23:31:16 +0200

    Update settings

--- project/settings.py
+++ project/settings.py
@@ -9,6 +9,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# big time TODO i guess
 SECRET_KEY = 'django-insecure-hm(^)4wgdxo8(_$h*un8(ux#x)4jhr9pbhj5fnqql%9n*d7v9$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
