3d112d2b024b0940bb58cc2736691ecc10383778
dylanmydude <dylanmethinks@gmail.com>
2025-02-16 19:21:09 +0200

    backgroung gradient colour correction

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -6,7 +6,8 @@ import TopBar from './TopBar';
 
 const Dashboard = () => {
   return (
-    <div className="flex h-screen w-screen bg-gradient-to-br from-[#020204] via-[#1f1764] via-[#050a13] via-[#101e3a] to-[#020204]">
+    <div className="flex h-screen w-screen bg-gradient-to-br from-[#010200] via-[#161a66] via-[#02040a] via-[#0c1e42] to-[#010200]">
+
       {/* Sidebar */}
       <div className="w-1/6 flex flex-col p-4 pt-10 backdrop-blur-md bg-black/20 border border-gray-800/20 shadow-md">
         <h2 className="text-6xl mb-9 text-white pl-4">Special</h2>
